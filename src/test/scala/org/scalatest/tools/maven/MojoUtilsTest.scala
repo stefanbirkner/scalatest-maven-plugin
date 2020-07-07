77e5c08feaf1fea1708f2c87bd7e349a7e3f771a
Stefan Birkner <mail@stefan-birkner.de>
2020-07-07 20:45:03 +0200

    Use System Lambda instead of System Rules
    System Lambda is independent of the testing framework. It allows the
    project to change to JUnit Jupiter or another testing framework.

--- src/test/scala/org/scalatest/tools/maven/MojoUtilsTest.scala
+++ src/test/scala/org/scalatest/tools/maven/MojoUtilsTest.scala
@@ -1,5 +1,6 @@
 package org.scalatest.tools.maven
 
+import com.github.stefanbirkner.systemlambda.SystemLambda._
 import org.junit.contrib.java.lang.system.{ClearSystemProperties, EnvironmentVariables}
 import org.junit.{Rule, Test}
 
@@ -15,29 +16,37 @@ class MojoUtilsTest {
 
   @Test
   def getJvmHappyPath(): Unit = {
-    env.clear("JAVA_HOME")
-    System.setProperty("java.home", "/test/jvm")
-    assert(MojoUtils.getJvm == "/test/jvm/bin/java")
+    withEnvironmentVariable("JAVA_HOME", null).execute(() -> {
+      env.clear("JAVA_HOME")
+      System.setProperty("java.home", "/test/jvm")
+      assert(MojoUtils.getJvm == "/test/jvm/bin/java")
+    })
   }
 
   @Test
   def getJvmWithoutJavaHome(): Unit = {
-    env.clear("JAVA_HOME")
-    assert(MojoUtils.getJvm == "java")
+    withEnvironmentVariable("JAVA_HOME", null).execute(() -> {
+      env.clear("JAVA_HOME")
+      assert(MojoUtils.getJvm == "java")
+    }
   }
 
   @Test
   def getJvmFromEnvironment(): Unit = {
-    env.clear("JAVA_HOME")
-    env.set("JAVA_HOME", "/opt/jdk-11")
-    Console.print(MojoUtils.getJvm)
-    assert(MojoUtils.getJvm == "/opt/jdk-11/bin/java")
+
+    withEnvironmentVariable("JAVA_HOME", null).execute(() -> {
+      env.clear("JAVA_HOME")
+      env.set("JAVA_HOME", "/opt/jdk-11")
+      Console.print(MojoUtils.getJvm)
+      assert(MojoUtils.getJvm == "/opt/jdk-11/bin/java")
+    }
   }
 
   @Test
   def getJvmJavaHomeIsPriority(): Unit = {
     System.setProperty("java.home", "/test/jvm")
-    env.set("JAVA_HOME", "/opt/jdk-11")
-    assert(MojoUtils.getJvm == "/opt/jdk-11/bin/java")
+    withEnvironmentVariable("JAVA_HOME", "/opt/jdk-11").execute(() -> {
+      assert(MojoUtils.getJvm == "/opt/jdk-11/bin/java")
+    }
   }
 }
